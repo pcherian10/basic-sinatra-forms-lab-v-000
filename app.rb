7b377ce03d4c3259d752c7f5bdb436d038edf557
Paul <pcherian10@gmail.com>
2018-06-20 04:52:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,7 +9,8 @@ class App < Sinatra::Base
   post '/team' do
     team_name = params["name"]
     coach = params["coach"]
-    team_name = params["pg"]
+    pg = params["pg"]
+    pg = params["pg"]
 
     erb :team
   end
